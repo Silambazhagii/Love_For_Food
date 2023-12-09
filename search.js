10bd793b20d75e82cb13f9ce838f8999acd5846d
Silambazhagii <silambazhagii@gmail.com>
2023-12-09 11:32:36 +0530

    update

--- search.js
+++ search.js
@@ -4,7 +4,6 @@ const ingredients = document.querySelector('#ing-btn');
 const searched_name = document.querySelector('.value');
 const rtarrow = document.querySelector('.rtarrow');
 const modal = document.getElementById("modal");
-const overlay = document.getElementById("overlay");
 
 let foodResults = [];
 let resultIndex = 0;
@@ -12,6 +11,9 @@ let resultIndex = 0;
 document.addEventListener('keyup', () => {
   const foodName = search.value;
   fetch_api(foodName);
+  if (foodName ==""){
+    window.location.reload()
+  }
 });
 
 ingredients.addEventListener('click', () => {
@@ -23,23 +25,19 @@ rtarrow.addEventListener('click', () => {
   updateImageAndName();
 });
 
-overlay.addEventListener("click", (event) => {
-  if (event.target === overlay) {
-    closeModal();
-  }
-});
-
 function fetch_api(foodName) {
   let url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${foodName}`;
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
-      foodResults = data.meals || [];
-      resultIndex = 0;
+      foodResults = data.meals ;
       fetch_data(data);
+      console.log(data)
     })
     .catch((err) => {
-      console.log(err.message);
+      if(foodName!='' && foodName.length>3){
+        alert(`There is no meal as ${foodName} `);
+      }
     });
 }
 
@@ -51,7 +49,7 @@ function fetch_data(data) {
     food_pic.src = imageSrc;
     searched_name.textContent = name;
   } else {
-    console.log("No meals found");
+    console.log('Error')
   }
 }
 
@@ -90,7 +88,7 @@ function displayPopup(meal, updateImage) {
   for (let i = 1; i <= 20; i++) {
     const ingredient = meal[`strIngredient${i}`];
     const measure = meal[`strMeasure${i}`];
-    if (ingredient && ingredient.trim() !== "") {
+    if (ingredient!== "") {
       ingredientsArray.push(`${ingredient}: ${measure}`);
     }
   }
@@ -99,12 +97,8 @@ function displayPopup(meal, updateImage) {
   popupText.textContent = `Ingredients for ${meal.strMeal}: ${ingredientsText}`;
 
   if (updateImage) {
-
     food_pic.src = meal.strMealThumb;
   }
-
-  modal.classList.add("from-right-arrow");
-
   openModal();
 }
 
@@ -112,6 +106,7 @@ function updateImageAndName() {
   if (foodResults.length > 0) {
     const name = foodResults[resultIndex].strMeal;
     const imageSrc = foodResults[resultIndex].strMealThumb;
+
     food_pic.src = imageSrc;
     searched_name.textContent = name;
   } else {
@@ -122,16 +117,9 @@ function updateImageAndName() {
 function closeModal() {
   modal.classList.remove("active");
   overlay.classList.remove("active");
-
-  if (!modal.classList.contains("from-right-arrow")) {
-    updateImageAndName();
-  }
-
-  modal.classList.remove("from-right-arrow");
 }
 
 function openModal() {
   modal.classList.add("active");
   overlay.classList.add("active");
 }
-
